bebe2bcefd3ccdac88cfe379842357c2cb970626
Leonardo Franca Almeida Silva <leofrancal17@gmail.com>
2025-08-12 17:00:21 -0300

    review

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',           // gera a pasta out
+  images: { unoptimized: true }
 };
 
 export default nextConfig;
