2f79289d3e9d0eee25d5f35f80dc88286ecd4b4e
Leonardo Franca Almeida Silva <leofrancal17@gmail.com>
2025-08-13 13:10:14 -0300

    teste

--- src/components/forms/newExpenseForm.tsx
+++ src/components/forms/newExpenseForm.tsx
@@ -129,7 +129,7 @@ export function NewExpenseForm({ onClose, onCreated }: Props) {
   return (
     <form
       onSubmit={handleSubmit}
-      className="space-y-4 text-white overflow-y-auto"
+      className="space-y-4 text-white overflow-y-auto max-h-[110dvh]"
     >
       {/* Descrição + Valor */}
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
